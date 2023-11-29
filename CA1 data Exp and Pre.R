841100fd5e4be28c174687c0cf06a478fb8618ee
Charles Franklin Jahn <72029865+Charlesjahn@users.noreply.github.com>
2023-11-29 23:18:29 +0000

    Update CA1 data Exp and Pre.R

--- CA1 data Exp and Pre.R
+++ CA1 data Exp and Pre.R
@@ -42,13 +42,37 @@ head(dataReorganized, 20)
 # Find the mean of all crimes based in the states and month of a specific crime 
 means_crimes <- dataReorganized %>%
   group_by(State, Month) %>%
-  summarise(across(c(Rape, Vehicle_Theft, Homicide, Bodily_injury_followed_by_death, Robbery_Institution, Cargo_Theft, Vehicle_Robbery, Robbery_Followed_by_Death, Attempted_Homicide), ~mean(., na.rm = TRUE), .names = "Mean_{.col}"))
+  summarise(across(c(Rape, 
+                     Vehicle_Theft, 
+                     Homicide, 
+                     Bodily_injury_followed_by_death, 
+                     Robbery_Institution, 
+                     Cargo_Theft, 
+                     Vehicle_Robbery, 
+                     Robbery_Followed_by_Death, 
+                     Attempted_Homicide), ~mean(., na.rm = TRUE), .names = "Mean_{.col}"))
         
 # Replace NA values in the crime columns with the corresponding mean values, keeping only the columns with the results.
 dataReorganized <- dataReorganized %>%
   left_join(means_crimes, by = c("State", "Month")) %>%
-  mutate(across(c(Rape, Vehicle_Theft, Homicide, Bodily_injury_followed_by_death, Robbery_Institution, Cargo_Theft, Vehicle_Robbery, Robbery_Followed_by_Death, Attempted_Homicide), ~ifelse(is.na(.x), get(paste0("Mean_", cur_column())), .x)),
-         across(c(Rape, Vehicle_Theft, Homicide, Bodily_injury_followed_by_death, Robbery_Institution, Cargo_Theft, Vehicle_Robbery, Robbery_Followed_by_Death, Attempted_Homicide), ~as.integer(round(.)))) %>%
+  mutate(across(c(Rape, 
+                  Vehicle_Theft, 
+                  Homicide, 
+                  Bodily_injury_followed_by_death, 
+                  Robbery_Institution, 
+                  Cargo_Theft, 
+                  Vehicle_Robbery, 
+                  Robbery_Followed_by_Death, 
+                  Attempted_Homicide), ~ifelse(is.na(.x), get(paste0("Mean_", cur_column())), .x)),
+         across(c(Rape, 
+                  Vehicle_Theft, 
+                  Homicide, 
+                  Bodily_injury_followed_by_death, 
+                  Robbery_Institution, 
+                  Cargo_Theft, 
+                  Vehicle_Robbery, 
+                  Robbery_Followed_by_Death, 
+                  Attempted_Homicide), ~as.integer(round(.)))) %>%
   select(State, Year, Month, Rape, Vehicle_Theft, Homicide, Bodily_injury_followed_by_death, Robbery_Institution, Cargo_Theft, Vehicle_Robbery, Robbery_Followed_by_Death, Attempted_Homicide)
 
 # View updated data
