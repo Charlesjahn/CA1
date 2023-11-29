a9b8d6de5fcfe27660e095bc4bb1ffe63d7e2089
Charles Franklin Jahn <72029865+Charlesjahn@users.noreply.github.com>
2023-11-29 15:58:41 +0000

    Update CA1 data Exp and Pre.R

--- CA1 data Exp and Pre.R
+++ CA1 data Exp and Pre.R
@@ -14,14 +14,32 @@ dataReorganized <- dataCrimes %>%
 
 head(dataReorganized, 20)
 
-# Find a mean based in the UF (States) and Mes (Month)
+#Translate the database titles from Portugues to English
+dataReorganized <- dataReorganized %>%
+  rename(
+    State = UF,
+    Year = Ano,
+    Month = Mês,
+    Rape = Estupro,
+    Vehicle_Theft = `Furto de veículo`,
+    Homicide = `Homicídio doloso`,
+    Bodily_injury_followed_by_death = `Lesão corporal seguida de morte`,
+    Robbery_Institution = `Roubo a instituição financeira`,
+    Cargo_Theft = `Roubo de carga`,
+    Vehicle_Robbery = `Roubo de veículo`,
+    Robbery_Followed_by_Death = `Roubo seguido de morte (latrocínio)`,
+    Attempted_Homicide = `Tentativa de homicídio`
+  )
+
+head(dataReorganized, 20)
+# Find a mean based in the States and Month
 dataLong <- dataReorganized %>%
-  select(-Ano) %>%
-  group_by(UF, Mês) %>%
-  summarise(Mean = round(mean(Estupro, na.rm = TRUE)))
+  select(-Year) %>%
+  group_by(State, Month) %>%
+  summarise(Mean = round(mean(Rape, na.rm = TRUE)))
 
 head(dataLong)
 
 # add a new column to sum all crimes for each line
 dataReorganized <- dataReorganized %>%
-  mutate(`Total Crimes` = rowSums(select(., -UF, -Ano, -Mês), na.rm = TRUE))
+  mutate(Total_Crimes = rowSums(select(., -State, -Year, -Month), na.rm = TRUE))
