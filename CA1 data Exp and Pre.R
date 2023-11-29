1454e5dbf0ac31942d6c41f0f19c49bc15e0554a
Charles Franklin Jahn <72029865+Charlesjahn@users.noreply.github.com>
2023-11-29 14:02:58 +0000

    Update CA1 data Exp and Pre.R

--- CA1 data Exp and Pre.R
+++ CA1 data Exp and Pre.R
@@ -1,3 +1,14 @@
 getwd()
 setwd("C:/Users/charl/Desktop/classes/Data Exploration and Preparation/CA1")
+library(tidyr)
+# read the file
+dataCrimes <- read.csv("C:/Users/charl/Desktop/classes/Data Exploration and Preparation/CA1/Crimes_in_Brazil_2015_2022.csv")
 
+# Show 20 first
+head(dataCrimes, 20)
+
+# Reorganize the data
+dataReorganized <- dataCrimes %>%
+  pivot_wider(names_from = `Tipo.Crime`, values_from = Ocorrências)
+
+head(dataReorganized, 20)
